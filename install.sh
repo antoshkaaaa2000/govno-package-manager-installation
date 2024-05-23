e173d2b87d923c4d70cf469ecbef92c04a4c5b3c
antoshkaaaa2000 <148551570+antoshkaaaa2000@users.noreply.github.com>
2024-05-23 20:15:05 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+cd
+sudo pacman -Suy
+sudo pacman -S git dkms
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+sudo rm -rf yay
+git clone https://github.com/antoshkaaaa2000/svoZOV
+cd svoZOV
+sudo cp * $HOME
+cd
+sudo rm -rf svoZOV
+sudo chmod 777 govno.sh
+sudo chmod 777 govno-remove.sh
